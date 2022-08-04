6c3953d7c3d3ae74aac3cb592ecc81025fa529cf
Rogério Fontes <fontestz@gmail.com>
2022-08-04 20:50:13 -0300

    :rocket: dcominio nos services

--- src/service/ClienteFisicoService.java
+++ src/service/ClienteFisicoService.java
@@ -8,11 +8,27 @@ import java.util.List;
 public class ClienteFisicoService {
     List<ClienteFisico> clienteFisicoList = new ArrayList<>();
 
-    public void salvar(ClienteFisico clienteFisico) {
+    public List<ClienteFisico> salvar(ClienteFisico clienteFisico) {
         if (!clienteFisico.validarNumeroDocument()) {
             System.out.println("Ele nao ẽ cpf");
         }
 
-        clienteFisicoList.add(clienteFisico);
+        clienteFisicoList.add(clienteFisico); // tarefa 1
+
+        //TODO - enviar um email de cadatro - tarefa 2
+        enviaEmail(clienteFisico.getEmail());
+
+        //return clienteFisicoList; //tarefa 3
+        return listaCliente();
     }
+
+    public void enviaEmail(String email) {
+        System.out.println("Envia email para: " + email);
+    }
+
+
+    public List<ClienteFisico> listaCliente() {
+        return clienteFisicoList;
+    }
+
 }
